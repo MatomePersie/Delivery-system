674476fcc379517e9d2db1a32479af94428b4c91
PersieMatome <mmphaphudi@sts3d.co.za>
2025-04-24 11:05:07 +0200

    added menu and cart

---
+++ script.js
@@ -0,0 +1,16 @@
+function addToCart(name, price){
+
+    let cart= JSON.parse(localStorage.getItem("cart")) || [];
+
+    const existingItem= cart.find(item=>item.name ===  name);
+    if(existingItem){
+        existingItem.quantity++;
+
+    }
+    else{
+        cart.push({name,price,quantity:1});
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert('${name} added to cart!');
+}
